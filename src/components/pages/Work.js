c4dac01e8d1e8793a2d26cac6eecd2cdb117ef6b
krish-vijayan <krishvijayan55@gmail.com>
2022-08-18 18:23:39 -0400

    Updated work page

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -2,31 +2,18 @@ import React from 'react';
 import '../../App.css';
 
 function Work() {
-	return (
-		<>
-			<div className='work-top'>
-				<h1 className='work-font'>Work</h1>
-			</div>
-			<div className='work-bottom'>
-				<h2>Skills</h2>
-				<ul className='work-list'>
-					<li>React JS, Node JS, REST, GraphQL</li>
-					<li>Adobe Experience Manager</li>
-					<li>CSS, JavaScript, HTML</li>
-					<li>Netlify, Heroku, GitHub</li>
-					<li>AutoCAD, SolidWorks</li>
-					<li>Adobe Photoshop, Adobe Premiere Pro</li>
-					<li>UX/UI, performance checking, documentation</li>
-				</ul>
-
-				<h2>Work Experience</h2>
-				<ul className='work-list'>
-					<li>Thomson Reuters (JR Architect Intern)</li>
-					<li>Elections Ontario (Resource Staff/Tech Support, Supervisor)</li>
-				</ul>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="work-top">
+        <h1 className="work-font">Work</h1>
+      </div>
+      <div className="work-bottom">
+        <div className="resume-background">
+          <img className="resume" src="/images/resume-work.png" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Work;
