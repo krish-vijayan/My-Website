17821bcb6f96d0a61a2401c719ce382e40245756
krish-vijayan <krishvijayan55@gmail.com>
2022-08-28 18:39:19 -0400

    Added interests

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Button } from "./Button";
 import "./HeroSection.css";
 import "../App.css";
+import ParticlesBackground from "./ParticlesBackground";
 
 function HeroSection() {
   return (
-    <div className="hero-container">
-      <video src="/videos/bAw_w1.mp4" autoPlay loop muted />
-      <h1>Krish Vijayan</h1>
-      <p> 2A Mechatronics Engineering Student</p>
+    <>
+      <div className="particles">
+        <ParticlesBackground />
+      </div>
 
-      <img src="/images/pfp.jpg" className="pfp" />
-    </div>
+      <div className="hero-container">
+        {/* <video src="/videos/bAw_w1.mp4" autoPlay loop muted />  */}
+        <h1>Krish Vijayan</h1>
+        <p> 2A Mechatronics Engineering Student</p>
+
+        <img src="/images/pfp.jpg" className="pfp" />
+      </div>
+    </>
   );
 }
 
--- src/components/pages/Interests.js
+++ src/components/pages/Interests.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+import {
+  ScrollContainer,
+  ScrollPage,
+  Animator,
+  Sticky,
+  batch,
+  Fade,
+  Zoom,
+  StickyIn,
+  FadeIn,
+  ZoomIn,
+  ZoomOut,
+  MoveIn,
+  MoveOut,
+  FadeOut,
+  Move,
+  StickyOut,
+} from "react-scroll-motion";
 function Interests() {
   return (
     <motion.div
@@ -11,7 +28,76 @@ function Interests() {
       <div className="interests-top">
         <h1 className="interests-font">Interests</h1>
       </div>
-      <div className="interests-bottom">Hello</div>
+      <div className="interests-bottom">
+        <ScrollContainer>
+          <ScrollPage page={0}>
+            <Animator animation={batch(StickyIn())}>
+              <h1 style={{ fontSize: "5vw" }}>
+                New Technology <i class="fa-solid fa-microchip"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={1}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Programming <i class="fa-solid fa-code"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={2}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Hockey <i class="fa-solid fa-hockey-puck"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={3}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Cricket <i class="fa-solid fa-i"></i>
+                <i class="fa-solid fa-p"></i>
+                <i class="fa-solid fa-l"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={4}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Basketball <i class="fa-solid fa-basketball"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={5}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Baseball <i class="fa-solid fa-baseball-bat-ball"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+
+          <ScrollPage page={5}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Driving <i class="fa-solid fa-car"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={5}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                TV & Movies <i class="fa-solid fa-tv"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage page={5}>
+            <Animator animation={StickyOut()}>
+              <h1 style={{ fontSize: "5vw" }}>
+                Video Games <i class="fa-solid fa-gamepad"></i>
+              </h1>
+            </Animator>
+          </ScrollPage>
+        </ScrollContainer>
+      </div>
     </motion.div>
   );
 }
